2a6880184221844707e9a4e9bf02f59e0557f1f6
andrejs <andrejss88@gmail.com>
2020-02-23 12:23:32 +0100

    refactor word meaning extraction

--- data-prep/src/main/java/com/wordup/ExtractWordMeanings.java
+++ data-prep/src/main/java/com/wordup/ExtractWordMeanings.java
@@ -3,13 +3,14 @@ package com.wordup;
 import com.wordup.utils.FileUtils;
 
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 import static com.wordup.utils.FileUtils.convertFileToStringList;
 import static java.lang.String.join;
+import static java.util.Arrays.asList;
+import static java.util.Arrays.copyOfRange;
 import static java.util.stream.Collectors.toList;
 
 public class ExtractWordMeanings {
@@ -29,12 +30,13 @@ public class ExtractWordMeanings {
         String listDelim = ",";
 
         for (String line : rawContent) {
+
             String word = extractWord(line);
             String definition = extractDefinition(line);
             List<String> synonyms = extractSynonyms(line);
             List<String> examples = extractExamples(line);
 
-            // we only want words with all data present
+            // only keep the words with all data present
             if (!definition.isEmpty() && !synonyms.isEmpty() && !examples.isEmpty()) {
                 sb.append(word).append(delim)
                         .append(definition).append(delim)
@@ -47,6 +49,24 @@ public class ExtractWordMeanings {
         FileUtils.write(sb.toString(), Paths.get(OUTPUT_PATH + "preparedDict.txt"));
     }
 
+    /**
+     * Given the below input example:
+     *
+     * @param line: 02738211 43 v 02 shine 0 beam 3 | emit light; be bright, as of the sun or a light; "The sun shone bright that day";
+     * @return substring the 5th space-divided element
+     */
+    private static String extractWord(String line) {
+        return line.split("\\s")[4];
+    }
+
+    /**
+     * Given the below input example:
+     * 02738211 43 v 02 shine 0 beam 3 | emit light; be bright, as of the sun or a light; "The sun shone bright that day";
+     * <p>
+     * 1) Get the 4th space-divided element N (02) - this is how many total synonyms we can expect in this line
+     * 2) If it's N < 2 - this means no synonyms, so we skip
+     * 3) Otherwise get N-1 elements starting from the first synonym (5th element) that we use as the base word
+     */
     private static List<String> extractSynonyms(String line) {
         String[] splitLine = line.split("\\s");
         String synonymNum = splitLine[3];
@@ -66,11 +86,36 @@ public class ExtractWordMeanings {
         int firstPossibleSynonymIndex = 6;
         int numOfSynonyms = synonyms - 1;
         int lastPossibleSynonymIndex = firstPossibleSynonymIndex + numOfSynonyms * 2 - 1;
-        List<String> syns = Arrays.asList(Arrays.copyOfRange(splitLine, firstPossibleSynonymIndex, lastPossibleSynonymIndex));
+        List<String> syns = asList(copyOfRange(splitLine, firstPossibleSynonymIndex, lastPossibleSynonymIndex));
 
-        return syns.stream()
+        return syns.stream() // e.g. 0 word1 0 word2 0 word3
                 .filter(s -> s.length() > 1) // hackily remove digits
-                .map(s -> s.replaceAll("_", " "))
+                .map(s -> s.replaceAll("_", " ")) // some words are like_that
+                .collect(toList());
+    }
+
+    /**
+     * Given the below input example:
+     *
+     * @param line: 02738211 43 v 02 shine 0 beam 3 | emit light; be bright, as of the sun or a light; "The sun shone bright that day";
+     * @return substring between '|' and the first occurrence of ';'
+     */
+    private static String extractDefinition(String line) {
+        String half = line.split("\\|")[1].trim();
+        return half.substring(0, half.indexOf(';'));
+    }
+
+    /**
+     * Given the below input example:
+     *
+     * @param line: 02738211 43 v 02 shine 0 beam 3 | emit light; be bright, as of the sun or a light; "The sun shone bright that day";
+     * @return substring all text between quotes, e.g. "example 1" and "example 2"
+     */
+    private static List<String> extractExamples(String line) {
+        return Pattern.compile(TEXT_IN_QUOTES_REGEX)
+                .matcher(line)
+                .results()
+                .map(MatchResult::group)
                 .collect(toList());
     }
 
@@ -81,26 +126,5 @@ public class ExtractWordMeanings {
         } catch (NumberFormatException e) {
             return false;
         }
-    }
-
-    private static String extractDefinition(String line) {
-        String half = line.split("\\|")[1].trim();
-        return half.substring(0, half.indexOf(';'));
-    }
-
-    private static List<String> extractExamples(String line) {
-        return extractSubstring(line, TEXT_IN_QUOTES_REGEX);
-    }
-
-    private static List<String> extractSubstring(String line, String regex) {
-        return Pattern.compile(regex)
-                .matcher(line)
-                .results()
-                .map(MatchResult::group)
-                .collect(toList());
-    }
-
-    private static String extractWord(String line) {
-        return line.split("\\s")[4];
     }
 }
